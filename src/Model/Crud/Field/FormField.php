0a1860b2519634e343e52180b467fc63ea483c79
Sipos Zoltán <sipiszoty@gmail.com>
2025-04-14 12:10:30 +0200

    configure admin forms to validate form datas

--- src/Model/Crud/Field/FormField.php
+++ src/Model/Crud/Field/FormField.php
@@ -2,6 +2,8 @@
 
 namespace App\Model\Crud\Field;
 
+use Symfony\Component\Validator\Constraint;
+
 class FormField
 {
     public function __construct(
@@ -9,6 +11,7 @@ class FormField
         private string $label,
         private string $type = 'text',
         private array  $parameters = [],
+        private array  $constraints = [],
     )
     {
     }
@@ -31,5 +34,13 @@ class FormField
     public function getParameters(): array
     {
         return $this->parameters;
+    }
+
+    /**
+     * @return array<Constraint>
+     */
+    public function getConstraints(): array
+    {
+        return $this->constraints;
     }
 }
